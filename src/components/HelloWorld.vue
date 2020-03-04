e7a67effb6e2b0373d9ba124db6d865169e583ec
Zhang Yuzheng <me@zhanyuzheng.com>
2020-03-04 15:09:12 +0800

    QuantzUi:feat:enable highstock
    Signed-off-by: Zhang Yuzheng <me@zhanyuzheng.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container>
+    <highcharts :constructor-type="'stockChart'" :options="stockOptions"></highcharts>
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
@@ -92,10 +93,28 @@
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
 
   data: () => ({
+    stockOptions: {
+      rangeSelector: {
+        selected: 1
+      },
+      title: {
+        text: 'AAPL Stock Price'
+      },
+      series: [{
+        name: 'AAPL',
+        data: [10, 20, 10, 23, 65, 121, 44, 66, 98, 30, 32, 56, 25, 12, 53],
+        pointStart: Date.UTC(2018, 1, 1),
+        pointInterval: 1000 * 3600 * 24,
+        tooltip: {
+          valueDecimals: 2
+        }
+      }]
+    },
     ecosystem: [
       {
         text: 'vuetify-loader',
--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import HighchartsVue from 'highcharts-vue'
+import Highcharts from 'highcharts'
+import stockInit from 'highcharts/modules/stock'
+
+stockInit(Highcharts)
 
 Vue.config.productionTip = false
+Vue.use(HighchartsVue)
 
 new Vue({
   router,
